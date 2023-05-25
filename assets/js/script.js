c66b337fc7fc071d4be9cddea080f5bb07cda4aa
martaleixo <128098209+martaleixo@users.noreply.github.com>
2023-05-25 22:08:43 +0100

    js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/*alert("funcionando")
+
+var js = "amazing";
+console.log(js)*/
+
+
+var anchor = document.querySelector("#main-navigation").querySelectorAll("a");
+
+console.log(anchor);
+for(var i = 0; i < anchor.length; i++) {
+    if(window.location.href === anchor[i].href) {
+        anchor[i].classList.add("active");
+    }
+
+    if (window.location.href.indexOf("about") > -1) {
+        document.body.classList.add("about");
+    } else if (window.location.href.indexOf("contacts") > -1) {
+        document.body.classList.add("contacts");
+    } else {
+        document.body.classList.add("home");
+    }
+}
